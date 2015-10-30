187faa963e215b264edc093999940284287a304e
Dinna Truong <dinnatruong@gmail.com>
2015-10-30 10:48:56 -0400

    Included descriptions of the code

--- core/src/com/mygdx/game/TexturePacker.java
+++ core/src/com/mygdx/game/TexturePacker.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
-// OPTION 1 (less smooth than option 2)
+// OPTION 1 (less smooth than option 2 - loops through sprites too fast)
 public class TexturePacker extends ApplicationAdapter {
 	SpriteBatch batch;
 	TextureAtlas taBomberman;
@@ -17,10 +17,11 @@ public class TexturePacker extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		taBomberman = new TextureAtlas(Gdx.files.internal("bomber.txt"));
-		spBomberman = new Sprite[7];
-
-		for(i = 0; i < 7; i++) {
+		//Create an array sprites loaded from the TextureAtlas
+		taBomberman = new TextureAtlas(Gdx.files.internal("bomberman.txt"));
+		spBomberman = new Sprite[4];
+		//Loop through regions of the TextureAtlas and assign each to an index of the array
+		for(i = 0; i < 4; i++) {
 			this.spBomberman[i] = new Sprite(this.taBomberman.findRegion("frame_" + i));
 		}
 	}
@@ -30,19 +31,22 @@ public class TexturePacker extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		nSpeed++;
-		if(nSpeed%30 == 0) {
-			if (i == 7) {
+		//Draw sprites on to screen
+		//nSpeed changes the time interval at which the sprites are drawn
+//		nSpeed++;
+//		if(nSpeed%7 == 0) {
+			if (i == 4) {
 				i = 0;
 			} else {
 				spBomberman[i++].draw(batch);
 			}
-		}
+//		}
 		batch.end();
 	}
 }
 
-/* OPTION 2: http://www.gamefromscratch.com/post/2015/02/27/LibGDX-Video-Tutorial-Sprite-Animation.aspx
+/* OPTION 2: Using the animation class
+	http://www.gamefromscratch.com/post/2015/02/27/LibGDX-Video-Tutorial-Sprite-Animation.aspx
 
 	SpriteBatch batch;
 	private TextureAtlas textureAtlas;
