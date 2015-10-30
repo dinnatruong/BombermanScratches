006ac0831fe0e6d149b602cb3ed6542d113df0b9
Dinna Truong <dinnatruong@gmail.com>
2015-10-30 01:21:37 -0400

    Included 2 options for animation

--- core/src/com/mygdx/game/TexturePacker.java
+++ core/src/com/mygdx/game/TexturePacker.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
+// OPTION 1 (less smooth than option 2)
 public class TexturePacker extends ApplicationAdapter {
 	SpriteBatch batch;
 	TextureAtlas taBomberman;
@@ -30,14 +31,43 @@ public class TexturePacker extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		nSpeed++;
-		if(nSpeed%20 == 0) {
+		if(nSpeed%30 == 0) {
 			if (i == 7) {
 				i = 0;
 			} else {
-				spBomberman[i].draw(batch);
-				i++;
+				spBomberman[i++].draw(batch);
 			}
 		}
 		batch.end();
 	}
 }
+
+/* OPTION 2: http://www.gamefromscratch.com/post/2015/02/27/LibGDX-Video-Tutorial-Sprite-Animation.aspx
+
+	SpriteBatch batch;
+	private TextureAtlas textureAtlas;
+	private Animation animation;
+	private float elapsedTime = 0f;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		textureAtlas = new TextureAtlas(Gdx.files.internal("bomber.pack"));
+		animation = new Animation(1f/15f, textureAtlas.getRegions());
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
+		textureAtlas.dispose();
+	}
+
+	@Override
+	public void render () {
+		elapsedTime += Gdx.graphics.getDeltaTime();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(animation.getKeyFrame(elapsedTime,true),0,0);
+		batch.end();
+	}
+}*/
